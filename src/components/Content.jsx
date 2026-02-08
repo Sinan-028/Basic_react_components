39cad0ba7963ac1c98242d5286bf3272aa71777c
Mahammad Sinan <sinanmhd909@gmail.com>
2026-02-08 10:42:37 +0530

    Initial commit: React simple components activity

---
+++ src/components/Content.jsx
@@ -0,0 +1,9 @@
+function Content() {
+  return (
+    <main className="content">
+      <p>This is a simple React application using functional components.</p>
+    </main>
+  );
+}
+
+export default Content;
